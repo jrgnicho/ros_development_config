8f2c3e24417ae2133c850e47e7067835dcedd075
ros-industrial <ros-industrial@rosindustrial-ThinkPad-T61p.(none)>
2014-01-08 11:27:38 -0600

    script for openning an ssh session window with multiple tabs

---
+++ general/ssh_session.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+IP=""
+USER=""
+PROFILE="Default"
+
+
+# check arguments
+if [[ ( "$#" -lt 2 )]]; then
+	echo "must pass user name and ip as follows: ssh_session.bash username ipaddress"
+	return
+else
+	IP=$2
+	USER=$1
+fi 
+
+if [[ ( "$#" -ge 3 )]]; then
+	PROFILE=$3
+fi
+
+
+# concatenate inputs
+FULL_ID="$USER@$IP"
+
+# construct command
+ARG="--tab-with-profile=$PROFILE --command='ssh -X $FULL_ID'"
+COMMAND="gnome-terminal $ARG $ARG $ARG $ARG $ARG $ARG $ARG"
+
+eval $COMMAND
