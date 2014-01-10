c62975dbb218b79e176cc2facb30f00ce8662c37
ros-industrial <ros-industrial@rosindustrial-ThinkPad-T61p.(none)>
2014-01-10 15:00:34 -0600

    Removed check for ros environment variables, changed ROS_VER variable for ROS_DISTRO

--- ros/hydro/ros_user_libs_setup.bash
+++ ros/hydro/ros_user_libs_setup.bash
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-
+ROS_DISTRO="hydro"
 # ros industrial training setup (rosbuild)
 #export USER_LIBRARY_PATH="$HOME/Desktop/Projects/ROS/ROS_Industrial_Training/ros_industrial_training/training/ref":$USER_LIBRARY_PATH
 #export USER_LIBRARY_PATH="$HOME/Desktop/Projects/ROS/ROS_Industrial_Training/ros_industrial_training/training/supplements":$USER_LIBRARY_PATH
 #source "$HOME/Desktop/Projects/ROS/ROS_Industrial_Training/ros_industrial_training/training/.training_units.bash" # training script
 
 # catkin workspaces 
-script="$HOME/ros/$ROS_VER/catkin_ws/devel/setup.bash"
+script="$HOME/ros/$ROS_DISTRO/catkin_ws/devel/setup.bash"
 if [ -f "$script" ]; then
 	source "$script"
 fi
 
 # rosbuild stack/package directories 
-USER_LIBRARY_PATH="$HOME/ros/$ROS_VER/rosbuild"
+USER_LIBRARY_PATH="$HOME/ros/$ROS_DISTRO/rosbuild"
 
 # Setting ROS PACKAGE PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$USER_LIBRARY_PATH
--- ros/hydro/setup.bash
+++ ros/hydro/setup.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-export ROS_VER="hydro"
-source "$HOME/linux_config/ros/$ROS_VER/ros_core_setup.bash"
-#source "$HOME/linux_config/ros/$ROS_VER/ros_android_setup.bash"
-source "$HOME/linux_config/ros/$ROS_VER/ros_user_libs_setup.bash"
+ROS_DISTRO="hydro"
+source "$HOME/linux_config/ros/$ROS_DISTRO/ros_core_setup.bash"
+#source "$HOME/linux_config/ros/$ROS_DISTRO/ros_android_setup.bash"
+source "$HOME/linux_config/ros/$ROS_DISTRO/ros_user_libs_setup.bash"
+PS1="ros-$ROS_DISTRO: "
