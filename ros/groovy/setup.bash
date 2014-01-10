27059d7a5ea8c87443edabcc7e2c754e591a8f29
ros-industrial <ros-industrial@rosindustrial-ThinkPad-T61p.(none)>
2014-01-10 14:59:10 -0600

    changed ROS_VER variable name for ROS_DISTRO

--- ros/groovy/setup.bash
+++ ros/groovy/setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-export ROS_VER="groovy"
-source "$HOME/linux_config/ros/$ROS_VER/ros_core_setup.bash"
-source "$HOME/linux_config/ros/$ROS_VER/ros_user_libs_setup.bash"
-source "$HOME/linux_config/ros/$ROS_VER/ros_android_setup.bash"
+ROS_DISTRO="groovy"
+source "$HOME/linux_config/ros/$ROS_DISTRO/ros_core_setup.bash"
+source "$HOME/linux_config/ros/$ROS_DISTRO/ros_user_libs_setup.bash"
+source "$HOME/linux_config/ros/$ROS_DISTRO/ros_android_setup.bash"
+PS1="ros-$ROS_DISTRO: "
