7784faea8c84bee69f62b5148eef89aae7d9a7e5
ros-industrial <ros-industrial@rosindustrial-ThinkPad-T61p.(none)>
2014-01-10 14:57:59 -0600

    removed check for ros environment variable

--- ros/groovy/ros_core_setup.bash
+++ ros/groovy/ros_core_setup.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 # checking if ros setup bash script was run
-if [ -z "$ROS_PACKAGE_PATH" ]; then
-	source "/opt/ros/$ROS_VER/setup.bash"
-	export ROS_WORKSPACE="/opt/ros/$ROS_VER/stacks"
-	echo "ROS $ROS_VER has been setup"
-fi
+ROS_DISTRO="groovy"
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+export ROS_WORKSPACE="/opt/ros/$ROS_DISTRO/stacks"
+echo "ROS $ROS_DISTRO is ready"
 
 
--- ros/groovy/ros_user_libs_setup.bash
+++ ros/groovy/ros_user_libs_setup.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ROS_DISTRO="groovy"
 
 # ros industrial (rosbuild)
 #export USER_LIBRARY_PATH="$HOME/Desktop/Projects/ROS/ROS_Industrial/subversion/trunk/swri-ros-pkg":$USER_LIBRARY_PATH
@@ -11,12 +12,15 @@
 #source "$HOME/Desktop/Projects/ROS/ROS_Industrial_Training/ros_industrial_training/training/.training_units.bash" # training script
 
 # catkin workspaces (Overlays packages previously defined in the ROS_PACKAGE_PATH)
-script="$HOME/ros/$ROS_VER/catkin_ws/devel/setup.bash"
+script="$HOME/ros/$ROS_DISTRO/catkin_ws/devel/setup.bash"
 if [ -f "$script" ]; then
 	source "$script" 
 fi
 
+# rosbuild stack/package directories 
+USER_LIBRARY_PATH="$HOME/ros/$ROS_DISTRO/rosbuild"
+
 # Setting ROS PACKAGE PATH
-#export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$USER_LIBRARY_PATH
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$USER_LIBRARY_PATH
 
 echo "USER_LIBRARY_PATH has been added to the ros path"
