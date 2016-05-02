2141358b929bc54d8e4f031379bff457dd78cbc4
jrgnicho <jrgnichodevel@gmail.com>
2016-05-02 15:28:03 -0500

    catkin_make_eclipse command takes make build type argument, defaults to 'Debug' when no argument is provide

--- general/ros_command_shortcuts.bash
+++ general/ros_command_shortcuts.bash
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-# command alias list
+# functions
+function __catkin_make_eclipse__()
+{
+  build_flag=$1
+  if [ -z "$build_flag" ]; then
+    build_flag="Debug"
+  fi
 
-# Builds project file for the Eclipse CDT IDE
-alias catkin_make_eclipse='catkin_make -DCMAKE_BUILD_TYPE=Debug --force-cmake -G"Eclipse CDT4 - Unix Makefiles" && awk -f $(rospack find mk)/eclipse.awk build/.project > build/.project_with_env && mv build/.project_with_env build/.project'
+  catkin_make -DCMAKE_BUILD_TYPE=$build_flag --force-cmake -G"Eclipse CDT4 - Unix Makefiles" && awk -f $(rospack find mk)/eclipse.awk build/.project > build/.project_with_env && mv build/.project_with_env build/.project
+}
+
+
+### command alias list
+alias catkin_make_eclipse='__catkin_make_eclipse__'
 
 
