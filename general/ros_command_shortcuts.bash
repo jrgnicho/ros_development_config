e3eebae2f44eb28780dbb6f83e29ea1665405edb
geoffreychiou <geoffrey.chiou@gmail.com>
2017-04-07 14:18:28 -0500

    catkin eclipse setup can now be done on individual packages

--- general/ros_command_shortcuts.bash
+++ general/ros_command_shortcuts.bash
@@ -11,29 +11,55 @@ function __catkin_make_eclipse__()
   catkin_make -DCMAKE_BUILD_TYPE=$build_flag --force-cmake -G"Eclipse CDT4 - Unix Makefiles" && awk -f $(rospack find mk)/eclipse.awk build/.project > build/.project_with_env && mv build/.project_with_env build/.project
 }
 
-function __catkin_eclipse_setup__()
+function __catkin_build_eclipse__()
 {
   build_flag=$1
   if [ -z "$build_flag" ]; then
     build_flag="Debug"
-  fi
+  fi 
 
-  catkin build  -c --force-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=$build_flag
-  __eclipse_project_files_gen__
+  
+
+  if [ "$#" -eq 0 ]; then # build entire workspace
+   catkin build -c --force-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=$build_flag
+    __eclipse_project_files_gen__
+  else
+
+    for pkg in "$@"
+    do
+      catkin build "$pkg" -c --force-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=$build_flag
+
+      # entering package build dir
+      build_dir=`catkin locate -b`
+      pkg_build_dir="$build_dir/$pkg"
+      cd "$pkg_build_dir"
+
+      # copying template into package build directory
+      echo "Creating eclipse project file at $pkg_build_dir"
+      awk -f $(rospack find mk)/eclipse.awk .project > .project_with_env && mv .project_with_env .project
+
+      # returning to workspace
+      cd "$build_dir/.."
+      
+    done
+    
+  fi
+  
 
 }
 
-function __eclipse_project_files_gen__()
+# generate project files for the entire workspace
+function __eclipse_project_files_gen__() 
 {
-  ROOT=$PWD 
-  cd build
+  build_dir=`catkin locate -b`
+  cd "$build_dir"
   for PROJECT in `find $PWD -name .project`; do
       DIR=`dirname $PROJECT`
       echo $DIR
       cd $DIR
       awk -f $(rospack find mk)/eclipse.awk .project > .project_with_env && mv .project_with_env .project
   done
-  cd $ROOT
+  cd "$build_dir/.."
 }
 
 function __catkin_build__all__()
@@ -62,7 +88,7 @@ function __catkin_build__pkg__()
 ### command alias list
 alias catkin_gen_eclipse_project='__eclipse_project_files_gen__'
 alias catkin_make_eclipse='__catkin_make_eclipse__'
-alias catkin_build_eclipse='__catkin_eclipse_setup__'
+alias catkin_build_eclipse='__catkin_build_eclipse__'
 alias cb='__catkin_build__all__'
 alias cbpkg='__catkin_build__pkg__'
 
