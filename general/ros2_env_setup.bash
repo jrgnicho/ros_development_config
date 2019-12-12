129691a1ee13f4d59c8b051bf82be3779cc12147
Jorge Nicho <jrgnichodevel@gmail.com>
2019-12-12 10:46:49 -0600

    Dashing support (#3)
    * added ros2 scripts and eclipse project generation files
    
    * added autocompletion support and aliases for ros2 scripts
    
    * bringing up existing workspace is functional, ros2 workspace creating needs work
    
    * ros2 workspace creation works now
    
    * added new profile and layout for ros2 workspaces

---
+++ general/ros2_env_setup.bash
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# variables
+ROS2_WS="ROS2_WS"
+
+# check arguments
+if [[ ( "$#" -lt 1 )]]; then
+	echo "$(tput setaf 1)must pass name of ros distribution (groovy, hydro, indigo, etc) $(tput sgr0)"
+	exit -1
+else
+	ROS2_DISTRO=$1
+fi 
+
+if [[ ( "$#" -ge 2 )]]; then
+	ROS2_WS=$2
+fi
+
+# ros workpace setup variables
+ROS2WS_DIR="$HOME/ros2/$ROS2_DISTRO/$ROS2_WS"
+
+# check ros2 workspace directory
+if [ ! -d "$ROS2WS_DIR" ]; then
+	echo "$(tput setaf 1)The ros2 workspace ($ROS2WS_DIR) does not exits $(tput sgr0)"
+	exit -1
+fi
+
+# ros system setup script
+source "/opt/ros/$ROS2_DISTRO/setup.bash"
+
+# ros2 workspace setup script (default)
+ROS2WS_SOURCE_SCRIPT="$ROS2WS_DIR/install/local_setup.bash"
+
+
+if [ -f "$ROS2WS_SOURCE_SCRIPT" ]; then
+	source "$ROS2WS_SOURCE_SCRIPT"
+else
+	echo "$(tput setaf 1)Error sourcing the ros2 workspace setup script $ROS2WS_SOURCE_SCRIPT$(tput sgr0)"
+fi
+
+# rosbuild workspace setup
+USER_LIBRARY_PATH="$HOME/ros/$ROS2_DISTRO/rosbuild"
+
+# setting up ros environment variables
+export MAKEFLAGS="-j1"
+export ROS_WORKSPACE="$ROS2WS_DIR"
+export ROS_LOCATIONS="ws=$ROS2WS_DIR:src=$ROS2WS_DIR/src:ros_development_config=$HOME/ros_development_config"
+export ROSCONSOLE_CONFIG_FILE="$ROS2WS_DIR/rosconsole.config"
+export ROS_PARALLEL_JOBS="-j2 -l2"
+export PYTHONPATH="$PYTHONPATH:$ROS2WS_DIR/src"
+export ROSCONSOLE_FORMAT='[${severity}]: ${message};'
+alias ros2ws_source="source $ROS2WS_SOURCE_SCRIPT"
+
+# check directory for optional configuration scripts
+OPTIONAL_CONF_SCRIPT="$LINUX_CONF_PATH/ros/$ROS2_DISTRO/setup.bash"
+if [ -f "$OPTIONAL_CONF_SCRIPT" ]; then
+	echo "$(tput setaf 3)Sourcing optional configuration script '$OPTIONAL_CONF_SCRIPT' $(tput sgr0)"
+  source "$OPTIONAL_CONF_SCRIPT"
+fi
+
+cd "$ROS2WS_DIR"
+PS1="ros2-$ROS2_DISTRO: "
+echo "$(tput setaf 3)ROS $ROS2_DISTRO is ready$(tput sgr0)"
--- general/ros2_session.bash
+++ general/ros2_session.bash
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-ROS_DISTRO=""
+ROS2_DISTRO=""
 PROFILE="Default"
-CATKIN_WS="catkin_ws"
+ROS2_WS="ros2_ws"
 ROS_SYSTEM_PATH="/opt/ros"
 CREATE_NEW_WS=false
-BUILD_TOOLS=("catkin-tools" "catkin_make") #(catkin-tools catkin_make)
+BUILD_TOOLS=("colcon")
 SELECTED_BUILD_TOOL="${BUILD_TOOLS[0]}"
 TERMINAL_OPTIONS=("terminator" "mate-terminal")
 TERMINAL_SELECTION="${TERMINAL_OPTIONS[0]}"
 
 SHORT_OPTIONS="r:w:l:p:c::h"
-LONG_OPTIONS="ros-distro:,workspace:,list-workspaces:,profile:,create::,help"
+LONG_OPTIONS="ros2-distro:,workspace:,list-workspaces:,profile:,create::,help"
 HELP_TEXT="Usage:\n
--r|--ros-distro [ros-distro] -w|--workspace [workspace name]\n
--l|--list-workspaces [ros-distro]\n
--c|--ros-distro [ros-distro] -w|--workspace [workspace name] --create\n
+-r|--ros2-distro [ros2-distro] -w|--workspace [workspace name]\n
+-l|--list-workspaces [ros2-distro]\n
+-c|--ros2-distro [ros2-distro] -w|--workspace [workspace name] --create\n
 -h|--help \n"
 
 function select_build_tool()
@@ -47,12 +47,12 @@ function check_supported_ros_distro()
 	fi
 }
 
-function check_catkin_workspace_exists()
+function check_ros2_workspace_exists()
 {
-	catkin_dir="$HOME/ros/$2/$1"
+	ros2_dir="$HOME/ros2/$2/$1"
 
 	# check directory
-	if [ ! -d "$catkin_dir" ]; then		
+	if [ ! -d "$ros2_dir" ]; then		
 		return 1 
 	else
 		return 0
@@ -62,7 +62,7 @@ function check_catkin_workspace_exists()
 function get_workspace_list()
 {
 	ros_distro=$1
-	ros_dir="$HOME/ros/$1"
+	ros_dir="$HOME/ros2/$1"
 	local -a a=()
 
 	# check for valid directory
@@ -73,15 +73,10 @@ function get_workspace_list()
 	# iterating through directories
 	counter=0
 	for d in $(ls -d $ros_dir/*); do
-		catkin_ws=$(basename $d)
-
-		# skip rosbuild
-		if [ $catkin_ws == "rosbuild" ]; then
-			continue
-		fi
+		ros2_ws=$(basename $d)
 
 		# add ws to array
-		a[$counter]=$catkin_ws
+		a[$counter]=$ros2_ws
 		let counter=counter+1
 
 	done
@@ -89,10 +84,10 @@ function get_workspace_list()
 	echo ${a[@]}
 }
 
-function list_catkin_workspaces()
+function list_ros2_workspaces()
 {
 	ros_distro=$1
-	ros_dir="$HOME/ros/$1"
+	ros_dir="$HOME/ros2/$1"
 
 	# check for valid directory
 	if [ ! -d "$ros_dir" ]; then
@@ -125,23 +120,23 @@ function main()
 	while true; do
 		OPTARG=$2		
 		case "$1" in
-			-r|--ros-distro)
-				#echo "ros-distro selected $OPTARG"
-				ROS_DISTRO=$OPTARG
+			-r|--ros2-distro)
+				#echo "ros2-distro selected $OPTARG"
+				ROS2_DISTRO=$OPTARG
 				shift 2
 				;;
 
 			-w|--workspace)
-				#echo "catkin workspace selected $OPTARG"
-				CATKIN_WS=$OPTARG
+				#echo "ros2 workspace selected $OPTARG"
+				ROS2_WS=$OPTARG
 				shift 2
 				;;
 
 			-l|--list-workspaces)
 
-        ROS_DISTRO=$OPTARG        
-				if(! list_catkin_workspaces $ROS_DISTRO); then
-					echo "ros $2 has no catkin workspaces"
+        ROS2_DISTRO=$OPTARG        
+				if(! list_ros2_workspaces $ROS2_DISTRO); then
+					echo "ros $2 has no ros2 workspaces"
 					exit 1
 				else
 					exit 0
@@ -186,14 +181,14 @@ function main()
 		let OPTIND=OPTIND+1
 	done
 
-	# positional arg ros-distro
+	# positional arg ros2-distro
 	if [ $# -ge 2 ]; then
-		ROS_DISTRO=$2
+		ROS2_DISTRO=$2
 	fi
 
 	# positional arg workspace
 	if [ $# -ge 3 ]; then
-		CATKIN_WS=$3
+		ROS2_WS=$3
 	fi
 
 		# positional arg profile
@@ -202,30 +197,25 @@ function main()
 	fi
 
 	# check ros distro
-	if(! check_supported_ros_distro $ROS_DISTRO); then
-		echo "$(tput setaf 1)ROS Distribution '$ROS_DISTRO' was not found$(tput sgr0)"
+	if (! check_supported_ros_distro $ROS2_DISTRO); then
+		echo "$(tput setaf 1)ROS2 Distribution '$ROS2_DISTRO' was not found$(tput sgr0)"
 		exit 1
 	fi
 
-	# check catkin workspace
-	if (! check_catkin_workspace_exists $CATKIN_WS $ROS_DISTRO); then
-
-	    if $CREATE_NEW_WS ; then
-	
+	if (! check_ros2_workspace_exists $ROS2_WS $ROS2_DISTRO); then
+	    if $CREATE_NEW_WS ; then	
 	      # checking that required variables have been set
-	      if [ "$ROS_DISTRO" == "" ] ; then
+	      if [ "$ROS2_DISTRO" == "" ] ; then
 	        echo "$(tput setaf 1)ROS distribution has not been set; recommended use:$(tput sgr0)"
-	        echo "$(tput setaf 1)ros_session -c [ros-distro] [catkin_ws]:$(tput sgr0)"
+	        echo "$(tput setaf 1)ros2_session -c [ros2-distro] [ros2_ws]:$(tput sgr0)"
 	        exit 1
 	      fi
 	
-	      ROSBUILD_DIR="$HOME/ros/$ROS_DISTRO/rosbuild"
-	
 	      # workspace creation
-	      source "$HOME/ros_development_config/general/ros_create_workspace.bash" $ROS_DISTRO $CATKIN_WS $SELECTED_BUILD_TOOL $ROSBUILD_DIR
+	      source "$HOME/ros_development_config/general/ros2_create_ws.bash" $ROS2_DISTRO $ROS2_WS $SELECTED_BUILD_TOOL
 	      
 	    else
-			  echo "$(tput setaf 1)Catkin workspace $CATKIN_WS for ros $ROS_DISTRO was not found$(tput sgr0)"
+			  echo "$(tput setaf 1)ROS2 workspace $ROS2_WS for ros $ROS2_DISTRO was not found$(tput sgr0)"
 			  exit 1
 	    fi
   	fi
@@ -245,11 +235,11 @@ function launch_terminator_terminal()
 {
 	TERMINAL_CMD="terminator"
 	cp -f ~/.bashrc ${LINUX_CONF_PATH}/bashrc.tmp
-	echo "source $LINUX_CONF_PATH/general/ros_core_setup.bash $ROS_DISTRO $CATKIN_WS">>"$LINUX_CONF_PATH/bashrc.tmp"
-	echo "echo -e \"\033]0;ROS-$ROS_DISTRO [$CATKIN_WS]\007\"">>"$LINUX_CONF_PATH/bashrc.tmp" # set title
-	echo "export PS1=\"ROS-$ROS_DISTRO[$CATKIN_WS]: \"&& clear">>"$LINUX_CONF_PATH/bashrc.tmp"
-	echo "echo \"$(tput setaf 3)ROS catkin workspace [$CATKIN_WS] is ready$(tput sgr0)\"">>"$LINUX_CONF_PATH/bashrc.tmp"
-	COMMAND="$TERMINAL_CMD -g $LINUX_CONF_PATH/general/terminator_config -l ros_devel"
+	echo "source $LINUX_CONF_PATH/general/ros2_env_setup.bash $ROS2_DISTRO $ROS2_WS">>"$LINUX_CONF_PATH/bashrc.tmp"
+	echo "echo -e \"\033]0;ROS-$ROS2_DISTRO [$ROS2_WS]\007\"">>"$LINUX_CONF_PATH/bashrc.tmp" # set title
+	echo "export PS1=\"ROS-$ROS2_DISTRO[$ROS2_WS]: \"&& clear">>"$LINUX_CONF_PATH/bashrc.tmp"
+	echo "echo \"$(tput setaf 3)ROS2 workspace [$ROS2_WS] is ready$(tput sgr0)\"">>"$LINUX_CONF_PATH/bashrc.tmp"
+	COMMAND="$TERMINAL_CMD -g $LINUX_CONF_PATH/general/terminator_config -l ros2_devel"
 	# the terminator configuration file has been set to execute the "bashrc.temp" script on each new terminal
 	eval $COMMAND & >> /dev/null
 	return 0
@@ -260,8 +250,8 @@ function launch_mate_terminal()
 	TERMINAL_CMD="mate-terminal"
 	# construct bash file
 	cp -f ~/.bashrc ${LINUX_CONF_PATH}/bashrc.tmp
-	echo "source $LINUX_CONF_PATH/general/ros_core_setup.bash $ROS_DISTRO $CATKIN_WS">>"$LINUX_CONF_PATH/bashrc.tmp"
-  TERMINAL_TAB_ARGS="--title='ros-$ROS_DISTRO: $CATKIN_WS' --profile=$PROFILE --command='bash --rcfile $LINUX_CONF_PATH/bashrc.tmp'"
+	echo "source $LINUX_CONF_PATH/general/ros2_env_setup.bash $ROS2_DISTRO $ROS2_WS">>"$LINUX_CONF_PATH/bashrc.tmp"
+  TERMINAL_TAB_ARGS="--title='ros-$ROS2_DISTRO: $ROS2_WS' --profile=$PROFILE --command='bash --rcfile $LINUX_CONF_PATH/bashrc.tmp'"
 	NEW_TAB_ARG="--tab $TERMINAL_TAB_ARGS"
 	COMMAND="$TERMINAL_CMD --window $TERMINAL_TAB_ARGS $NEW_TAB_ARG $NEW_TAB_ARG $NEW_TAB_ARG $NEW_TAB_ARG $NEW_TAB_ARG $NEW_TAB_ARG $NEW_TAB_ARG"
 
