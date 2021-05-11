33e7b6a4458594fd3c2f95e4925bc83e864c1b05
Jorge Nicho <jrgnichodevel@gmail.com>
2021-05-11 11:43:31 -0500

    does not install catkin nor colcon but throws warning

--- general/install_prerequisites.bash
+++ general/install_prerequisites.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-REQUIRED_PACKAGES=("terminator" "python-catkin-tools")
+INSTALL_PACKAGES=("terminator")
+REQUIRED_PACKAGES=("python-catkin-tools" "python3-colcon-common-extensions")
 
 function check_debian()
 {
   pkg=$1
+  do_install=$2 #should be bool
   apt-cache show $pkg &> /dev/null
   if [ $? -eq 0 ] ; then
     echo "package \"$pkg\" was found" &> /dev/null 
@@ -12,7 +14,13 @@ function check_debian()
     # check if it's installed
     dpkg -s $1 &> /dev/null  
     if [ $? -eq 1 ];
-    then      
+    then  
+      
+      if [ $do_install -eq 0 ] ; then
+        echo "Required package \"$pkg\" not installed"
+        return
+      fi
+          
       echo "Installing package \"$pkg\""
       sudo apt-get install $pkg 
     else
@@ -29,10 +37,16 @@ function check_debian()
 
 function main()
 {
+  for pkg in ${INSTALL_PACKAGES[*]}
+  do
+    check_debian $pkg 1
+
+  done
+  
   for pkg in ${REQUIRED_PACKAGES[*]}
   do
-	  #echo "check debian package \"$pkg\""
-    check_debian $pkg
+    #echo "check debian package \"$pkg\" 0"
+    check_debian $pkg 0
 
   done
 }
